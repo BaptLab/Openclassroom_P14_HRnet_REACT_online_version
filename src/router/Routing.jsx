986056253d8e2124f9d56550449671771a978bf6
Baptiste LABAUNE <baptiste.labaune@gmail.com>
2023-09-25 14:49:12 +0200

    g

--- src/router/Routing.jsx
+++ src/router/Routing.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, HashRouter } from "react-router-dom";
+import { Routes, Route, HashRouter, BrowserRouter } from "react-router-dom";
 
 import Homepage from "../pages/Homepage";
 import Error404 from "../pages/Error404";
@@ -10,13 +10,13 @@ import Employees from "../pages/Employees";
  */
 const Routing = () => {
   return (
-    <HashRouter basename="/">
+    <BrowserRouter basename={"/"}>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/employees" element={<Employees />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
